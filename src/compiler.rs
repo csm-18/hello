f65373b2326376c07e9a168ba28d64ae55bc58b3
csubrahmanyam <csubrahmanyam2@gmail.com>
2024-10-16 13:23:24 +0530

    add lexer() function
    lexer() makes tokens out of src code

--- src/compiler.rs
+++ src/compiler.rs
@@ -4,5 +4,19 @@
 pub fn compile(src:&str) -> String {
     //output(assembly code)
     let output: String = "output!".to_owned();
+
+    //make tokens from src
+    let tokens = lexer(src);
+    dbg!(tokens);
+
     return output;
+}
+
+//creates tokens from source code
+fn lexer(src:&str) -> Vec<String>{
+    //tokens
+    let tokens: Vec<String> = vec!["tokens!".to_string(),];
+
+
+    return tokens;
 }
