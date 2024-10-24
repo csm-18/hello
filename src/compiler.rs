f9443465cf2728de60927c3c3786be3be2530649
csubrahmanyam <csubrahmanyam2@gmail.com>
2024-10-24 12:56:01 +0530

    change tokens from string type to custom token-enum type

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,5 +1,7 @@
 //compiler.rs : contains actual compiler code
 
+use std::process::exit;
+
 //converts hello code into assembly and returns it
 pub fn compile(src:&str) -> String {
     //output(assembly code)
@@ -10,10 +12,27 @@ pub fn compile(src:&str) -> String {
     output
 }
 
+
+
+
 //create tokens from source code
-fn lexer(src:&str) -> Vec<String> {
+#[derive(Debug)]
+enum Token {
+    START,
+    STR_LIT(String),     //string literal
+    LEFT_PAREN(String),  //left parenthesis
+    RIGHT_PAREN(String), //right parenthesis
+    LEFT_BRACE(String),
+    RIGHT_BRACE(String),
+    KEYWORD(String),
+    ID(String),          //identifier
+    END,
+}
+fn lexer(src:&str) -> Vec<Token> {
+
+
     //tokens
-    let mut tokens: Vec<String> = vec!["start".to_string(),];
+    let mut tokens: Vec<Token> = vec![Token::START,];
 
     let mut x = 0;
     while x < src.len() {
@@ -23,7 +42,8 @@ fn lexer(src:&str) -> Vec<String> {
             while y < src.len() {
                 if &src[y..y+1] == "`" && &src[y-1..y] != "\\" {
                     no_end_backtick = false;
-                    tokens.push(src[x..y+1].to_string());
+                    let token = Token::STR_LIT(src[x..y+1].to_string());
+                    tokens.push(token);
                     x = y;
                     break;
                 }
@@ -33,6 +53,7 @@ fn lexer(src:&str) -> Vec<String> {
                 let (line,error_at) = char_position(&src,x);
                 println!("Token Error at {error_at} on line {line}");
                 println!("String Literal Not Terminated!");
+                exit(1);
             }
         } else if &src[x..x+1] == "/" && &src[x+1..x+2] == "/" {
             let mut no_newline = true;
@@ -49,8 +70,27 @@ fn lexer(src:&str) -> Vec<String> {
                 return tokens;
             }
         } else if &src[x..x+1] == "(" || &src[x..x+1] == ")" || &src[x..x+1] == "{" || &src[x..x+1] == "}" {
-            tokens.push(src[x..x+1].to_string());
+            match &src[x..x + 1] {
+                "(" => tokens.push(Token::LEFT_PAREN("(".to_string())),
+                ")" => tokens.push(Token::RIGHT_PAREN(")".to_string())),
+                "{" => tokens.push(Token::LEFT_BRACE("{".to_string())),
+                "}" => tokens.push(Token::RIGHT_BRACE("}".to_string())),
+                _ => exit(1),
+            }
         }
+        // } else {
+        //     //An identifier is made of letters
+        //     let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_','0','1','2','3','4','5','6','7','8','9'];
+        //     let mut token_error = true;
+        //
+        //     let mut temp_word = "";
+        //
+        //
+        //     if(token_error){
+        //         println!("Token Error at {error_at} on line {line}.");
+        //         exit(1);
+        //     }
+        // }
         x += 1;
     }
 
@@ -78,3 +118,4 @@ fn char_position(text:&str,char_index:usize) -> (usize,usize) {
 
     (line_number, position)
 }
+
